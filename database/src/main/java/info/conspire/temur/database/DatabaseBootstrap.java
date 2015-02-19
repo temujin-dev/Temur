8a7f4fe2ef23e101ebe4c853913798e245a050e9
Temujin <temujin.dev@hushmail.com>
2015-02-19 14:58:42 +0000

    Add Hibernate files & preliminary database design documentation

---
+++ database/src/main/java/info/conspire/temur/database/DatabaseBootstrap.java
@@ -0,0 +1,14 @@
+package info.conspire.temur.database;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+/**
+ * Project Temur
+ *
+ * @author Temujin
+ */
+public class DatabaseBootstrap {
+
+
+}
--- src/main/java/info/conspire/temur/Temur.java
+++ src/main/java/info/conspire/temur/Temur.java
@@ -1,10 +1,15 @@
 package info.conspire.temur;
 
 
+
 import info.conspire.temur.network.NetworkBootstrap;
 import info.conspire.temur.util.Configuration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.hibernate.HibernateException;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.service.ServiceRegistry;
 
 
 /**
@@ -19,7 +24,23 @@ public class Temur {
     public static void main(String[] args) {
         logger.info("Booting up Temur");
         config = new Configuration("src/main/resources/temur.cfg");
+        bootHibernate();
         NetworkBootstrap.boot(config.getString("network.host"), config.getInt("network.port"));
 
     }
+
+    public static SessionFactory bootHibernate() {
+        try {
+            org.hibernate.cfg.Configuration configuration = new org.hibernate.cfg.Configuration();
+            configuration.configure();
+            ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+            SessionFactory sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+            return sessionFactory;
+        }
+        catch (HibernateException ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+
 }
